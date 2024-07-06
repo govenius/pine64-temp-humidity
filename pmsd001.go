7f6cb592d5bf6abfcb97cd884ef557a4bb313f80
Joonas Govenius <joonas.govenius@gmail.com>
2024-07-06 20:45:11 +0300

    Add comman line arguments for specifying i2c bus/address, as well as an alternative JSON output format.

--- pmsd001.go
+++ pmsd001.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"golang.org/x/exp/io/i2c"
 	"time"
+	"flag"
 )
 
 const (
@@ -53,11 +54,25 @@ func getRelativeHumidity(d i2c.Device) float64 {
 }
 
 func main() {
-	d, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x40)
+
+	// Define and parse comman line arguments
+	i2cBus := flag.String("i", "/dev/i2c-1", "i2c bus to query (default: /dev/i2c-1)")
+	i2cAddress := flag.Int("a", 0x40, "i2c address to query (default: 0x40)")
+	jsonOutput := flag.Bool("j", false, "Output JSON instead of tab separated data (default: false)")
+	sensorName := flag.String("n", "pi", "sensor name included in JSON output (default: pi)")
+	flag.Parse()
+
+	d, err := i2c.Open(&i2c.Devfs{Dev: *i2cBus}, *i2cAddress)
 	check(err)
 
 	tempF, tempC := getTemp(*d)
 	humidity := getRelativeHumidity(*d)
 
-	fmt.Printf("%d\t%.2f\t%.2f\t%.2f\n", time.Now().Unix(), tempC, tempF, humidity)
+	if *jsonOutput {
+		fmt.Printf("{\"%s\": ", *sensorName)
+		fmt.Printf("{\"time\": %d, \"temperature\": %.2f , \"humidity\": %.2f}", time.Now().Unix(), tempC, humidity)
+		fmt.Printf("}\n")
+	} else {
+		fmt.Printf("%d\t%.2f\t%.2f\t%.2f\n", time.Now().Unix(), tempC, tempF, humidity)
+	}
 }
